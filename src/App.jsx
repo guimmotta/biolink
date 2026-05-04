2a1dc401dbbd10c92ec6686548009f0ef30f2972
guimmotta <aguinaldoammotta@gmail.com>
2026-05-04 12:07:10 -0300

    added save the date page along with styiling to said page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { store } from './store'
 import BioPage from './components/BioPage'
+import SaveTheDatePage from './components/SaveTheDatePage'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <BioPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BioPage />} />
+          <Route path="/save-the-date" element={<SaveTheDatePage />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   )
 }
---
+++ src/components/SaveTheDatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/main.css'
+
+export default function SaveTheDatePage() {
+  return (
+    <div className="save-the-date-page">
+      <h1 className="save-the-date-title">Save the Date</h1>
+      <p className="save-the-date-text">More details coming in June...</p>
+    </div>
+  )
+}
